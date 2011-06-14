8806f2865fec458cbc746fa16bdbba913cd7badd
Chach Sikes <chachasikes@gmail.com>
2011-06-13 22:26:05 -0700

    Polygons loading.

--- citygroups_apps/citygroups_blockwatch/js/map.js
+++ citygroups_apps/citygroups_blockwatch/js/map.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
 cityGroups.loadData = function() {
   var dataPath = "http://localhost/codeforamerica/citygroups/citygroups_map/data/community-groups-data.json";
-  
+
   var data = "";
-        
+
   $.ajax({
     url: dataPath,
     dataType: 'json',
@@ -21,7 +21,7 @@ cityGroups.loadData = function() {
     success: cityGroups.loadDataSuccess,
     error: cityGroups.loadDataError
   });
-  
+
   return false;
 };
 
@@ -33,22 +33,30 @@ cityGroups.loadDataError = function(data) {
 cityGroups.loadDataSuccess = function(data) {
   console.log("success!");
   cityGroups.data = data;
-  cityGroups.map.loadMap();  
+  cityGroups.map.loadMap();
   var mapObject = cityGroups.geoJSON(cityGroups.data.nodes);
 console.log(mapObject);
 
-	       var polygon = new L.Polygon(polygonPoints);
+
+/*
+  var polygon = new L.Polygon(mapObject, polygonOptions);
 	polygon.bindPopup("I am a polygon.");
 	map.addLayer(polygon);
-	
+*/
+
+
+
+
   return false;
 };
 
 cityGroups.geoJSON = function(nodes) {
+  var mapObject = [];
+  var polygonPoints = [];
+  var polygonPoint;
   var features = {};
   for (i in nodes) {
-  console.log($.parseJSON(nodes[i]["node"]["location_geo"]));
-  var locationGeoObj = $.parseJSON(nodes[i]["node"]["location_geo"]);
+    var locationGeoObj = $.parseJSON(nodes[i]["node"]["location_geo"]);
     switch(locationGeoObj.type) {
       case "Point":
 /*
@@ -58,30 +66,43 @@ cityGroups.geoJSON = function(nodes) {
       break;
       case "Polygon":
         var polygonPoints = Array();
-        for (p in locationGeoObj.coordinates) {
-          var polygonPoint = new L.LatLng(locationGeoObj.coordinates[0][p][0], locationGeoObj.coordinates[0][p][1]);
-          polygonPoints.push(polygonPoint);
+
+        for (p in locationGeoObj.coordinates[0]) {
+            polygonPoint = p;
+            polygonPoint = new L.LatLng(locationGeoObj.coordinates[0][p][1], locationGeoObj.coordinates[0][p][0]);
+            polygonPoints.push(polygonPoint);
         }
+
+      polygonOptions = {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5
+      };
+      console.log(polygonPoints);
+      var polygon2 = new L.Polygon(polygonPoints,polygonOptions);
+
+  		polygon2.bindPopup("I am a polygon.");
+  		map.addLayer(polygon2);
 
       break;
     }
   }
-  var mapObject = Array();
+
   mapObject = polygonPoints;
   return mapObject;
 };
 
 cityGroups.map.loadMap = function() {
-  // initialize the map on the "map" div with a given center and zoom 
-  cityGroups.map.settings.zoom = 13;  
+  // initialize the map on the "map" div with a given center and zoom
+  cityGroups.map.settings.zoom = 11;
   cityGroups.map.settings.center = new L.LatLng(47.6061889, -122.3308133);
   // cityGroups.map.settings.center = new L.LatLng(cityGroups.map.settings.latitude, cityGroups.map.settings.longitude);
-      
-  var map = new L.Map('map', cityGroups.map.settings.center);
-  
+
+  map = new L.Map('map', cityGroups.map.settings.center);
+
   // create a CloudMade tile layer
   var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/b59bc8b09cd84af58fcef3019d84e662/997/256/{z}/{x}/{y}.png',
       cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
-  
-  map.setView(cityGroups.map.settings.center, cityGroups.map.settings.zoom).addLayer(cloudmade);
+  map.setView(cityGroups.map.settings.center, cityGroups.map.settings.zoom);
+/*   map.setView(cityGroups.map.settings.center, cityGroups.map.settings.zoom).addLayer(cloudmade); */
 };
